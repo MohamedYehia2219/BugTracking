193fa17c2870534aa5e09c268867326198f49214
MohamedYehia2219 <77175242+MohamedYehia2219@users.noreply.github.com>
2024-03-03 00:53:36 +0200

    bug model

---
+++ models/bug.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const BugSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 200,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 1000,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["To_Do", "Done", "In_Progress"],
+      default: "To_Do",
+    },
+    priority: {
+      type: String,
+      required: true,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    severity: {
+      type: String,
+      required: true,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    timeCreated: {
+      type: Date,
+      default: Date.now,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project", 
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category", 
+      required: true,
+    },
+    lastUpdatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    lastUpdatedAt: {
+      type: Date,
+    },
+});
+const BugModel = mongoose.model("Bug", BugSchema);
+
+// Validate bug
+function validateBug(obj) {
+  const schema = Joi.object({
+    title: Joi.string().trim().min(2).max(200).required(),
+    description: Joi.string().trim().min(5).max(1000).required(),
+    status: Joi.string().valid("To_Do", "Done", "In_Progress").required(),
+    priority: Joi.string().valid("Low", "Medium", "High").required(),
+    severity: Joi.string().valid("Low", "Medium", "High").required(),
+    creator: Joi.string().required(), // Assuming creator is the user's ID
+    timeCreated:Joi.date().required(),
+    project: Joi.string().required(), // Assuming project is the project's ID
+    category: Joi.string().required(), // Assuming category is the category's ID
+    lastUpdatedBy: Joi.string(), // Optional field
+    lastUpdatedAt: Joi.date(), // Optional field
+  });
+  return schema.validate(obj);
+}
+
+module.exports = {
+    BugModel,
+    validateBug,
+};
---
+++ models/bugScreens.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const BugScreensSchema = new mongoose.Schema({
+    bug:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Bug", 
+      required: true,
+    },
+    screen:{
+      type: String,
+    },
+  });
+const BugScreensModel = mongoose.model("BugScreens", BugScreensSchema);
+
+// Validate bug screens
+function validateBugScreens(obj) {
+  const schema = Joi.object({
+    bug: Joi.string().required(), // Assuming bug is the bug's ID
+    screen: Joi.string(),
+  });
+  return schema.validate(obj);
+}
+
+module.exports = {
+    BugScreensModel,
+  validateBugScreens,
+};
