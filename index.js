098b638127bc72627fbe090c60f2c99991bae9b5
MohamedYehia2219 <77175242+MohamedYehia2219@users.noreply.github.com>
2024-03-03 05:08:05 +0200

    index

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
-//mongo connection
-const connectToDB= require("./configration/db");
-connectToDB();
-
 //server listen on port
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.listen(PORT, () =>
   console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//mongo connection
+const connectToDB= require("./configration/db");
+connectToDB();
 
 //routes
 const {authRouter} = require("./routes/auth")
