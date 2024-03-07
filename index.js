27919ab495b59486e9404c49c08bf25b65d842fa
MohamedYehia2219 <77175242+MohamedYehia2219@users.noreply.github.com>
2024-03-07 23:07:26 +0200

    device token

--- index.js
+++ index.js
@@ -21,12 +21,16 @@ const {projectRouter} = require("./routes/project")
 const {bugRouter} = require("./routes/bug")
 const {categoryRouter} = require("./routes/category")
 const {commentRouter} =require("./routes/comment")
+const {notificationRouter} =require("./routes/notification")
+const {tokenRouter} =require("./routes/deviceToken")
 app.use("/auth",authRouter);
 app.use("/users", userRouter);
 app.use("/projects",projectRouter);
 app.use("/bugs", bugRouter);
 app.use("/categories",categoryRouter);
 app.use("/comments", commentRouter);
+app.use("/notifications", notificationRouter);
+app.use("/deviceTokens", tokenRouter);
 
 //static folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
--- routes/deviceToken.js
+++ routes/deviceToken.js
@@ -3,6 +3,33 @@ const tokenRouter = express.Router();
 const isAuthantecated=require("../middlewares/auth");
 const { TokenModel,validateTokens} = require("../models/device_token")
 
+tokenRouter.post("/",isAuthantecated,async(req,res)=>{
+    try{
+        const {error} = validateTokens(req.body);
+        if(error) {return res.status(200).json({ message: error.details[0].message, status:false });}
+        const {deviceToken} = req.body;
+        let user = await TokenModel.findOne({userId: req.userId});
+        if(!user)
+        {
+            let newRow = new TokenModel({userId: req.userId, deviceToken});
+            await newRow.save();
+            return res.status(200).json({ message:"Device Token added successfully...", status:true });
+        }
+        else
+        {
+            await TokenModel.findOneAndUpdate({userId: req.userId}, {userId: req.userId, deviceToken});
+            return res.status(200).json({ message:"Device Token updated successfully...", status:true });
+        }
+    }catch(error){return res.status(500).json({ message: error.message, status:false })} 
+})
 
-
+tokenRouter.get("/:id", async(req,res)=>{
+    try{
+        let user = await TokenModel.findOne({userId: req.params.id});
+        if(user)
+            return res.status(200).json({ data: {deviceToken: user.deviceToken}, status:true });
+        else
+            return res.status(200).json({ message:"User has not device token yet.. !!", status:true });
+    }catch(error){return res.status(500).json({ message: error.message, status:false })} 
+})
 module.exports={tokenRouter}
